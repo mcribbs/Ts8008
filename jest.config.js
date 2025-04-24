d3a09102653ea25e60efdeb4da9e3c27667fe870
Matt Cribbs <mcribbs@gmail.com>
2025-04-24 13:14:23 -0400

    Project setup with jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+};
